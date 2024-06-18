2426dbab49f95ce3112579d823fc6039f72d1cf2
shinhyunha <eistar9109@gmail.com>
2024-06-18 15:32:36 +0900

    dto 수정

--- src/main/java/com/mall/biz/member/dto/ReqSaveMemberDto.java
+++ src/main/java/com/mall/biz/member/dto/ReqSaveMemberDto.java
@@ -9,25 +9,31 @@ import lombok.NoArgsConstructor;
 @Data
 @Getter
 public class ReqSaveMemberDto {
-    @NotBlank(message = "현재 비밀번호는 {valid.notblank}")
+    @NotBlank(message = "로그인 아이디는 {valid.notblank}")
     @Schema(description = "로그인 아이디", type = "String")
     private String loginId;
 
+    @NotBlank(message = "비밀번호는 {valid.notblank}")
     @Schema(description = "비밀번호", type = "String")
     private String password;
 
+    @NotBlank(message = "이름은 {valid.notblank}")
     @Schema(description = "이름", type = "String")
     private String name;
 
+    @NotBlank(message = "나이는 {valid.notblank}")
     @Schema(description = "나이", type = "int")
     private int age;
 
+    @NotBlank(message = "전화번호는 {valid.notblank}")
     @Schema(description = "전화번호(-없이)", type = "String")
     private String phone;
 
+    @NotBlank(message = "주소는 {valid.notblank}")
     @Schema(description = "주소", type = "int")
     private String address;
 
+    @NotBlank(message = "우편번호는 {valid.notblank}")
     @Schema(description = "우편번호", type = "int")
     private String zipCode;
 }
